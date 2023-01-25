aefcfc99bb2ed1780e92f21d9d40f6cb7965d135
Sakurych <kolonka.power@mail.ru>
2023-01-25 14:09:06 +0400

    add lab 3

---
+++ cryeptoLab_3/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Encode = new System.Windows.Forms.Button();
+            this.Decode = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Encode
+            // 
+            this.Encode.Location = new System.Drawing.Point(108, 95);
+            this.Encode.Name = "Encode";
+            this.Encode.Size = new System.Drawing.Size(164, 163);
+            this.Encode.TabIndex = 0;
+            this.Encode.Text = "Зашифровать";
+            this.Encode.UseVisualStyleBackColor = true;
+            this.Encode.Click += new System.EventHandler(this.Encode_Click);
+            // 
+            // Decode
+            // 
+            this.Decode.Location = new System.Drawing.Point(313, 95);
+            this.Decode.Name = "Decode";
+            this.Decode.Size = new System.Drawing.Size(164, 163);
+            this.Decode.TabIndex = 1;
+            this.Decode.Text = "Расшифровать";
+            this.Decode.UseVisualStyleBackColor = true;
+            this.Decode.Click += new System.EventHandler(this.Decode_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(511, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 647);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Decode);
+            this.Controls.Add(this.Encode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Encode;
+        private System.Windows.Forms.Button Decode;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ cryeptoLab_3/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static byte[] img;
+        public static byte[] buffer1;
+        public static byte[] AllByte;
+        public static byte[] key;
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap btmp = (Bitmap)Image.FromFile("100.bmp");
+            pictureBox1.Image = btmp;
+        }
+
+        private void Encode_Click(object sender, EventArgs e)
+        {
+            Bitmap imge = new Bitmap("100.bmp");
+            byte[] img =  ConvertBitMapToByte(imge);
+            pictureBox1.Image = ConvertByteToBitMap(img);
+            using (FileStream fstream = File.OpenRead("text.txt"))
+            {
+                // выделяем массив для считывания данных из файла
+                key = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(key, 0, key.Length);
+            }
+            byte[] data = crypt.Encrypt(img, key);
+            Bitmap b = new Bitmap(ConvertByteToBitMap(data));
+            b.Save("Encode.bmp", ImageFormat.Bmp);
+            pictureBox1.Image = b;
+        }
+
+        private byte[] ConvertBitMapToByte(Bitmap img)
+        {
+            byte[] Result = null;
+            BitmapData bData = img.LockBits(new Rectangle(new Point(), img.Size), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            int ByteCount = bData.Stride * img.Height;
+            Result = new byte[ByteCount];
+            Marshal.Copy(bData.Scan0, Result, 0, ByteCount);
+            img.UnlockBits(bData);
+            return Result;
+        }
+
+        private Bitmap ConvertByteToBitMap(byte[] Ishod)
+        {
+            Bitmap img = new Bitmap(1152, 648);
+            BitmapData bData = img.LockBits(new Rectangle(new Point(), img.Size), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            Marshal.Copy(Ishod, 0, bData.Scan0, Ishod.Length);
+            img.UnlockBits(bData);
+            return img;
+        }
+
+        private void Decode_Click(object sender, EventArgs e)
+        {
+            Bitmap imge = (Bitmap)Image.FromFile("Encode.bmp");
+            ImageConverter imgCon = new ImageConverter();
+            img = ConvertBitMapToByte(imge);
+            using (FileStream fstream = File.OpenRead("text.txt"))
+            {
+                // выделяем массив для считывания данных из файла
+                key = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(key, 0, key.Length);
+            }
+            byte[] data = crypt.Encrypt(img, key);
+            Bitmap b = new Bitmap(ConvertByteToBitMap(data));
+            b.Save("Decode.bmp", ImageFormat.Bmp);
+            pictureBox1.Image = b;
+        }
+    }
+
+    internal class crypt
+    {
+        public static byte[] Encrypt(byte[] data, byte[] key)
+        {
+            int a, i, j, k, tmp;
+            int[] codeKey, box;
+            byte[] cipher;
+
+            codeKey = new int[256];
+            box = new int[256];
+            cipher = new byte[data.Length];
+
+            for (i = 0; i < 256; i++)
+            {
+                codeKey[i] = key[i % key.Length];
+                box[i] = i;
+            }
+            for (j = i = 0; i < 256; i++)
+            {
+                j = (j + box[i] + codeKey[i]) % 256;
+                tmp = box[i];
+                box[i] = box[j];
+                box[j] = tmp;
+            }
+            for (a = j = i = 0; i < data.Length; i++)
+            {
+                a++;
+                a %= 256;
+                j += box[a];
+                j %= 256;
+                tmp = box[a];
+                box[a] = box[j];
+                box[j] = tmp;
+                k = box[((box[a] + box[j]) % 256)];
+                cipher[i] = (byte)(data[i] ^ k);
+            }
+            return cipher;
+        }
+
+
+        public static byte[] Decrypt(byte[] data, byte[] key)
+        {
+            return Encrypt(data, key);
+        }
+    }
+}
